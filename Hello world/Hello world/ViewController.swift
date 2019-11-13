d3f9fa8e93c6c23f9d4c0b8b88f9e56ee284029c
Nick Nikulin <n9179001111@gmail.com>
2019-11-13 20:40:45 +0300

    Hello world

---
+++ Hello world/Hello world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by nic on 12.11.2019.
+//  Copyright © 2019 nic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
